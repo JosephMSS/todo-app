bfd8dc117e8f134ef8d8b32b10ca63e8f0819522
Joseph Morales Silva <josephzx1@hotmail.com>
2023-12-27 18:47:02 -0600

    Refactor HomeComponent to use Task model and add createTask and addTask methods

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,6 @@
 import { NgFor } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-interface Task {
-  title: string;
-  completed: boolean;
-}
-class Task implements Task {
-  constructor(title: string, completed: boolean) {
-    this.title = title;
-    this.completed = completed;
-  }
-}
+import { Task } from '../../models/task.model';
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-home',
@@ -20,26 +11,36 @@ class Task implements Task {
 })
 export class HomeComponent {
   tasks = signal<Task[]>([
-    { title: 'Install Angular CLI', completed: true },
-    { title: 'Style app', completed: true },
-    { title: 'Finish service functionality', completed: false },
-    { title: 'Setup API', completed: false },
+    this.createTask('Install Angular CLI'),
+    this.createTask('Style app'),
+    this.createTask('Finish service functionality'),
+    this.createTask('Setup API'),
   ]);
   changeHandler(e: Event) {
     const { value } = e.target as HTMLInputElement;
-    const newTask = new Task(value, false);
-    /**
-     * *Los signals tienen métodos para actualizar sus valores,
-     * *ademas de darnos el estado anterior, debemos retornar un
-     * *nuevo estado y no mutar el anterior
-     *  */
-    this.tasks.update((tasks) => {
-      return [...tasks, newTask];
-    });
+    const newTask = this.createTask(value);
+    this.addTask(newTask);
   }
   deleteHandler(index: number) {
     this.tasks.update((tasks) =>
       tasks.filter((task, position) => position !== index)
     );
+  }
+  createTask(title: string) {
+    const id = Date.now().toString();
+    const newTask: Task = {
+      id,
+      title,
+      completed: false,
+    };
+    return newTask;
+  }
+  addTask(task: Task) {
+    /**
+     * *Los signals tienen métodos para actualizar sus valores,
+     * *ademas de darnos el estado anterior, debemos retornar un
+     * *nuevo estado y no mutar el anterior
+     *  */
+    this.tasks.update((tasks) => [...tasks, task]);
   }
 }
